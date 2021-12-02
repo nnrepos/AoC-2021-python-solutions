32049db87a9bad6ba75b4e05a943dd16c5ea13ae
NNrepos <nivimano@gmail.com>
2021-12-02 07:12:20 +0200

    part 2 done

--- src/solutions/02.py
+++ src/solutions/02.py
@@ -1,15 +1,38 @@
 from utils.utils import *
 
-lines = get_input(__file__) + "hi"
-numlines = lines_to_nums(lines)
+lines = get_input(__file__)
+slines = lines.splitlines()
 
 
 def part1():
-    pass
+    forw = 0
+    down = 0
+    for line in slines:
+        x, y = line.split()
+        if x == "forward":
+            forw += int(y)
+        if x == "down":
+            down += int(y)
+        if x == "up":
+            down -= int(y)
+    return forw * down
 
 
 def part2():
-    pass
+    forw = 0
+    down = 0
+    aim = 0
+    for line in slines:
+        x, y = line.split()
+        y = int(y)
+        if x == "forward":
+            forw += y
+            down += y * aim
+        if x == "down":
+            aim += y
+        if x == "up":
+            aim -= y
+    return forw * down
 
 
 print("part1:", part1())
